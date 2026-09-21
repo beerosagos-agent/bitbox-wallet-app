d3e5e3a4cde7444b5f409ff15707eab2ee05a9fc
beerosagos-agent <luca+agent@bitbox.swiss>
2026-09-21 09:55:54 +0000

    frontend: remove vendor iframe barrel exports

--- frontends/web/src/routes/market/pocket.tsx
+++ frontends/web/src/routes/market/pocket.tsx
@@ -21,14 +21,14 @@ import { MarketGuide } from './guide';
 import { convertScriptType } from '@/utils/request-addess';
 import { parseExternalBtcAmount } from '@/api/coins';
 import { FirmwareUpgradeRequiredDialog } from '@/components/dialog/firmware-upgrade-required-dialog';
+import { useMarketIframeActive } from '@/hooks/vendor-iframe-active';
+import { useVendorIframeResizeHeight } from '@/hooks/vendor-iframe-resize-height';
+import { useVendorTerms } from '@/hooks/vendor-iframe-terms';
 import {
   getVendorIframeMessageTarget,
   postMessageToVendorIframe,
-  useMarketIframeActive,
-  useVendorIframeResizeHeight,
-  useVendorTerms,
   type TVendorIframeMessageTarget,
-} from '@/hooks/vendor-iframe';
+} from '@/hooks/vendor-iframe-message';
 import { useAccountSynced } from '@/hooks/account';
 import { Message } from '@/components/message/message';
 import style from './iframe.module.css';
